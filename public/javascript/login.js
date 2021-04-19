d289b1d41a7427747ccf95f98df5c1299d4cc8c6
Brown Mamba <brdyherrera@gmail.com>
2021-04-19 00:04:55 -0400

    Cleaning up code

--- public/javascript/login.js
+++
@@ -1,7 +0,0 @@
-async function loginFormHandler(event) {
-    event.preventDefault();
-}
-
-document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
-
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
--- public/javascript/logout.js
+++
@@ -1,4 +0,0 @@
-async function logout() {}
-
-
-document.querySelector('#logout').addEventListener('click', logout);
